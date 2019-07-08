f19a47a2abbf46bd61db7be8984eaa4b98df9357
Filippo Finke <filippo.finke@ComuneChiasso.Locale>
2019-07-08 14:00:13 +0200

    Added option to hide gui

--- plates.py
+++ plates.py
@@ -4,10 +4,10 @@ from os.path import isfile, join
 import random
 import pytesseract
 import numpy as np
-IMAGES_PATH = './images/'
+import sys
+from argparse import ArgumentParser
 
-images = [f for f in listdir(IMAGES_PATH) if isfile(join(IMAGES_PATH, f))]
-print("Found " + str(len(images)) + " images!")
+IMAGES_PATH = './images/'
 
 def filter(input):
     valids = []
@@ -16,9 +16,26 @@ def filter(input):
             valids.append(character)
     return ''.join(valids)
 
+
+parser = ArgumentParser()
+parser.add_argument("-i", "--input", dest="image", help="Input image")
+parser.add_argument("-g", "--gui", help="Show the gui")
+
+args = parser.parse_args()
+if args.gui:
+    verbose = True
+else:
+    verbose = False
+
+if args.image:
+    images = [args.image]
+else:
+    images = [IMAGES_PATH + f for f in listdir(IMAGES_PATH) if isfile(join(IMAGES_PATH, f))]
+    print("Found " + str(len(images)) + " images!")
+
 for image in images:
 
-    original = cv2.imread(IMAGES_PATH + image)
+    original = cv2.imread(image)
 
     grayscale = cv2.cvtColor(original, cv2.COLOR_BGR2GRAY)
     mask = cv2.inRange(grayscale, 140,255)
@@ -37,11 +54,13 @@ for image in images:
                 text = pytesseract.image_to_string(plate)
                 if text != "":
                     plate_text = filter(text)
-                    cv2.imshow(plate_text, plate)
-                    cv2.rectangle(original, (x,y), (x+w,y+h), (0,255,0), 2)
-                    cv2.putText(original, plate_text, (x,y), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 3)
-
-    cv2.putText(original, "@filippofinke",(20, original.shape[0] - 20),cv2.FONT_HERSHEY_SIMPLEX,1,(0,255,0),3)
-    cv2.imshow('Image' ,original)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+                    print(plate_text)
+                    if verbose:
+                        cv2.imshow(plate_text, plate)
+                        cv2.rectangle(original, (x,y), (x+w,y+h), (0,255,0), 2)
+                        cv2.putText(original, plate_text, (x,y), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 3)
+    if verbose:
+        cv2.putText(original, "@filippofinke",(20, original.shape[0] - 20),cv2.FONT_HERSHEY_SIMPLEX,1,(0,255,0),3)
+        cv2.imshow('Image' ,original)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
